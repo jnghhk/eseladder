c3500f155256a33a9e82b458e4b86cafabe2238a
jnghhk <jnghhk@naver.com>
2017-12-02 00:48:55 +0900

    printmap.h

---
+++ ese/final/printmap.h
@@ -0,0 +1,21 @@
+#ifndef PRINTMAP_H
+#define PRINTMAP_H
+#include<stdio.h>
+#include"printColorString.h"
+void printmap(int **MAP,int line)
+{
+	char buf[128]={0,};
+	int i, j;
+	for (i = 0; i < 22; i++)
+	{
+		for (j = 0; j < line-1; j++)
+		{
+			sprintf(buf,"%c",MAP[i][j]);
+			if(j%2==0)	printColorString(1,buf);
+			else	printColorString(1,buf);
+		}
+		printf("\n");
+	}
+}
+
+#endif
